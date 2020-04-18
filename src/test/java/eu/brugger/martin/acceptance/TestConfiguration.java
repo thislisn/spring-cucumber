d19c414bb7fb6b2b5ab3d10ebcd8e11acdfec017
Oleksandr_Lisnychenk <Oleksandr_Lisnychenko@epam.com>
2020-04-18 17:01:48 +0300

    add destroy method

--- src/test/java/eu/brugger/martin/acceptance/TestConfiguration.java
+++ src/test/java/eu/brugger/martin/acceptance/TestConfiguration.java
@@ -1,6 +1,9 @@
 package eu.brugger.martin.acceptance;
 
 
+import eu.brugger.martin.acceptance.framework.driver.DriverProvider;
+import org.openqa.selenium.WebDriver;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -13,8 +16,16 @@ import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 @PropertySource(value = "classpath:testconfig.properties")
 public class TestConfiguration {
 
+    @Autowired
+    protected DriverProvider driverProvider;
+
     @Bean
     public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer(){
         return new PropertySourcesPlaceholderConfigurer();
+    }
+
+    @Bean(destroyMethod = "quit")
+    public WebDriver getWebDriver(){
+        return driverProvider.getInstance();
     }
 }
